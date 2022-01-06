15c6cd03dec68647375ba09f5e917dbc7fcdfd06
Dawid Owczarz <dawid.owczarz@ageno.pl>
2022-01-06 13:59:12 +0100

    07-finish

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,9 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import Post from "App/Models/Post";
+
+export const PostFactory = Factory.define(Post, ({ faker }) => {
+  return {
+    title: faker.lorem.sentence(),
+    content: faker.lorem.text()
+  }
+}).build()
