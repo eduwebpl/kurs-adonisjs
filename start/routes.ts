b11080ed0755eeec28d0ed83c38bc0c4b002d98a
Dawid Owczarz <dawid.owczarz@ageno.pl>
2022-01-06 12:47:51 +0100

    03-finish

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,6 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+Route.get('/posts/:id?', async ({ params }) => {
+  return params.id ? `This is post number ${params.id}` : `This is post page`
 })
