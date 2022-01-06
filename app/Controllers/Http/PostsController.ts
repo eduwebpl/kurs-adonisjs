687d02c353f171f812dfc068b846466a28cffe85
Dawid Owczarz <dawid.owczarz@ageno.pl>
2022-01-06 14:26:23 +0100

    09-finish

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,7 +1,12 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from "App/Models/Post";
 
 export default class PostsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ view }: HttpContextContract) {
+    const posts = await Post.all()
+
+    return view.render('welcome', { posts })
+  }
 
   public async create({}: HttpContextContract) {}
 
