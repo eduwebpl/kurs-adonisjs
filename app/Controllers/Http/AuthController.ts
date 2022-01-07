deb899f07398467bd87f6e4486e5c391c7d6f231
Dawid Owczarz <dawid.owczarz@ageno.pl>
2022-01-07 13:57:53 +0100

    26-finish

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,19 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import UserValidator from "App/Validators/UserValidator";
+import User from "App/Models/User";
+
+export default class AuthController {
+  public showRegister({ view }: HttpContextContract) {
+    return view.render('auth/register');
+  }
+
+  public async register({ request, auth, response }: HttpContextContract) {
+    const user = await request.validate(UserValidator)
+
+    const newUser = await User.create(user)
+
+    await auth.login(newUser)
+
+    return response.redirect().toRoute('/')
+  }
+}
